600c7daab41ca27153e039e0a32245f11fde9c79
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-11 22:33:58 +0900

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+module.exports = withMDX({
   reactStrictMode: true,
-}
+  pageExtentions: ["js", "jsx", "md", "mdx"],
+});
