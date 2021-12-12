5e63e28e0e25a0be09e54fda0595a80d2c39c080
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-12 15:36:56 +0900

    parse markdown

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,19 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import * as fs from "fs";
+// @ts-ignore "@mdx-js/mdx" is missing the official type definition
+import compile from "@mdx-js/mdx";
 import { MDXProvider } from "@mdx-js/react";
+import * as fs from "fs";
+import { MDXContent } from "mdx/types";
+import { GetStaticPaths, GetStaticProps } from "next";
+import path from "path";
+import { ParsedUrlQuery } from "querystring";
+
+// For `import { compile } from "@mdx-js/mdx";`
+//declare function compile(mdxContent: string, opts?: object): MDXContent;
+
+const postsDirectory = "./posts";
 
 const getAllPostIds = () => {
-  const fileNames = fs.readdirSync("./posts");
+  const fileNames = fs.readdirSync(postsDirectory);
 
   return fileNames.map((fileName) => {
     return { params: { id: fileName.replace(/\.md$/, "") } };
@@ -11,20 +21,41 @@ const getAllPostIds = () => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  console.log("getStaticPaths is called");
   const paths = getAllPostIds();
-  console.log(paths);
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+interface Props {
+  mdxContent?: MDXContent;
+}
+
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}) => {
   console.log("getStaticProps is called");
-  return { props: {} };
+  if (params && params.id) {
+    const fullPath = path.join(postsDirectory, params.id + ".md");
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const mdxContent: MDXContent = await compile(fileContents);
+
+    return {
+      props: {
+        mdxContent,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
 };
 
-const Post = (): JSX.Element => {
+const Post = ({ mdxContent }: Props): JSX.Element => {
   const components = {};
-
+  console.log(mdxContent);
   return (
     <MDXProvider components={components}>
       <div>post</div>
